63f0a800a1af94b62eef32c46b550aaef86b9d31
Pavel Umansky <pavelyman76@gmail.com>
2026-01-12 15:05:46 +0100

    upgrade logic for film searching

--- database/meilisearch_sync.py
+++ database/meilisearch_sync.py
@@ -16,7 +16,10 @@ def search_movies_meili(query, limit=20):
         client = get_meili_client()
         index = client.get_index('movies')
         
-        results = index.search(query, {'limit': limit})
+        results = index.search(query, {
+            'limit': limit,
+            'matchingStrategy': 'last'  
+        })
         
         movies = []
         for hit in results.get('hits', []):
@@ -50,19 +53,74 @@ def index_all_movies():
         
         index = client.index('movies')
         
-        # Configure searchable attributes
         index.update_settings({
-            'searchableAttributes': ['title', 'genre', 'year'],
+            # Priority order for searching
+            'searchableAttributes': [
+                'title',
+                'director',
+                'description',
+                'genre'
+            ],
+            
             'filterableAttributes': ['genre', 'year', 'rating'],
-            'sortableAttributes': ['year', 'rating', 'title']
+            'sortableAttributes': ['year', 'rating', 'title'],
+            
+            
+            'rankingRules': [
+                'words',
+                'typo',
+                'proximity',
+                'attribute',
+                'sort',
+                'exactness'
+            ],
+            
+            # Typo tolerance - finds misspelled words
+            'typoTolerance': {
+                'enabled': True,
+                'minWordSizeForTypos': {
+                    'oneTypo': 3,   # 1 typo allowed for 3+ char words
+                    'twoTypos': 6  # 2 typos allowed for 6+ char words
+                }
+            },
+            
+           
+            'stopWords': [
+                'the', 'a', 'an', 'and', 'or', 'but', 'in', 'on', 'at', 'to',
+                'for', 'of', 'with', 'by', 'from', 'as', 'is', 'was', 'are',
+                'were', 'been', 'be', 'have', 'has', 'had', 'do', 'does',
+                'did', 'will', 'would', 'could', 'should', 'it', 'its',
+                'this', 'that', 'these', 'those', 'i', 'you', 'he', 'she',
+                'we', 'they', 'what', 'which', 'who', 'where', 'when', 'how'
+            ],
+            
+            
+            'synonyms': {
+                'film': ['movie', 'picture'],
+                'movie': ['film', 'picture'],
+                'scary': ['horror', 'thriller'],
+                'horror': ['scary', 'thriller'],
+                'funny': ['comedy', 'humor'],
+                'comedy': ['funny', 'humor'],
+                'action': ['adventure', 'thriller'],
+                'romantic': ['romance', 'love'],
+                'romance': ['romantic', 'love'],
+                'sci-fi': ['science fiction', 'scifi'],
+                'scifi': ['science fiction', 'sci-fi'],
+                'science fiction': ['sci-fi', 'scifi'],
+                'crime': ['gangster', 'mafia', 'noir'],
+                'gangster': ['crime', 'mafia'],
+                'war': ['military', 'battle'],
+                'animated': ['animation', 'cartoon'],
+                'animation': ['animated', 'cartoon'],
+                'documentary': ['doc', 'docu'],
+                'biography': ['biopic', 'bio']
+            }
         })
         
         movies = get_all_movies()
-        
-        # Convert to list of dicts
         movies_list = [dict(movie) for movie in movies]
         
-        # Index in Meilisearch
         index.add_documents(movies_list)
         
         print(f"Indexed {len(movies_list)} movies to Meilisearch")
